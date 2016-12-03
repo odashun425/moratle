77d7831080c2bc5cf635d33ef7d590aea1f12251
Syun Oda <usr0300809@apgrp.local>
2016-12-03 23:36:19 +0900

    1203_23_ログインメール機能作成完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,8 +14,18 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => 'smtp.gmail.com',
+    :user_name => "odashun0505@gmail.com", #ご自身のgmailアドレス
+    :password => "o118800268", #ご自身のgmailアドレスのパスワード
+    :authentication => 'login',
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
