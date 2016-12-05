8d3256e0f911bcb479167303c0dd67f687cc072b
Syun Oda <usr0300809@apgrp.local>
2016-12-05 20:42:58 +0900

    161205_新規登録は終わったので次！

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161203124853) do
+ActiveRecord::Schema.define(version: 20161204111729) do
+
+  create_table "campus", force: :cascade do |t|
+    t.string   "classname",   limit: 255
+    t.text     "classdetail", limit: 65535
+    t.string   "class",       limit: 255
+    t.string   "teacher",     limit: 255
+    t.string   "university",  limit: 255
+    t.string   "department",  limit: 255
+    t.string   "course",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "reviews", force: :cascade do |t|
     t.string   "nickname",      limit: 255
@@ -28,12 +40,12 @@ ActiveRecord::Schema.define(version: 20161203124853) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "email",                  limit: 255,   default: "", null: false
+    t.string   "encrypted_password",     limit: 255,   default: "", null: false
     t.string   "reset_password_token",   limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.integer  "sign_in_count",          limit: 4,     default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
@@ -42,8 +54,12 @@ ActiveRecord::Schema.define(version: 20161203124853) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email",      limit: 255
-    t.datetime "created_at",                                      null: false
-    t.datetime "updated_at",                                      null: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.date     "birth"
+    t.string   "nickname",               limit: 255
+    t.text     "university",             limit: 65535
+    t.text     "sibougyokai",            limit: 65535
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
